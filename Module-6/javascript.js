e26e5072c6134fdcfc8ab0e6a9f68060aa0510a4
Lindsey Choi <lindsey.e.choi@gmail.com>
2021-11-28 19:04:25 -0700

    adding module 4,5,6 complete

---
+++ Module-6/javascript.js
@@ -0,0 +1,19 @@
+function change_height() {
+    console.log("hi");
+    document.getElementById("box").style.height = "350px";
+    };
+
+function change_blue() {
+    console.log("hello");
+    document.getElementById("box").style.backgroundColor = "blue";
+    };
+
+function change_fade() {
+    console.log("hi there");
+    document.getElementById("box").style.opacity = "0.1";
+    };
+
+function change_reset() {
+    console.log("hi again");
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px"
+    };
